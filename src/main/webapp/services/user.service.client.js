e14b499e4f79dad5f39b885cd5059193dce04372
Will Cohen <will.r.cohen@gmail.com>
2020-09-26 11:27:08 -0400

    feat: Add user mgmt to index cards

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,17 @@
+
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/wcohen/users';
+    var self = this;
+    function createUser(user) {}
+    function findAllUsers() {
+        fetch(this.url)
+    }
+    function findUserById(userId) {}
+    function updateUser(userId, user) {}
+    function deleteUser(userId) {}
+}
