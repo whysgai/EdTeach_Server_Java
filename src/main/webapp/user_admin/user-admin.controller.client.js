cd26433238f18e55a239ef8e220a16b4b4e4f8fb
Will Cohen <will.r.cohen@gmail.com>
2020-09-27 16:30:15 -0400

    Implement renderUser for single user display

--- src/main/webapp/user_admin/user-admin.controller.client.js
+++ src/main/webapp/user_admin/user-admin.controller.client.js
@@ -1,181 +1,196 @@
-let users = [
-    {
-        userid: '0000001',
-        username: 'wonder',
-        firstname: 'Diana',
-        lastname: 'Prince',
-        role: 'Faculty'
-    },
-    {
-        userid: '0000002',
-        username: 'super',
-        firstname: 'Clark',
-        lastname: 'Kent',
-        role: 'Faculty'
-    },
-    {
-        userid: '0000003',
-        username: 'bat',
-        firstname: 'Bruce',
-        lastname: 'Wayne',
-        role: 'Faculty'
-    }
-]
+(function () {
+    let users = [
+        {
+            userid: '0000001',
+            username: 'wonder',
+            firstname: 'Diana',
+            lastname: 'Prince',
+            role: 'Faculty'
+        },
+        {
+            userid: '0000002',
+            username: 'super',
+            firstname: 'Clark',
+            lastname: 'Kent',
+            role: 'Faculty'
+        },
+        {
+            userid: '0000003',
+            username: 'bat',
+            firstname: 'Bruce',
+            lastname: 'Wayne',
+            role: 'Faculty'
+        }
+    ]
 
-let tbody
-let template
-let clone
-let selectedUser
+    let tbody
+    let template
+    let clone
+    let selectedUser
 
 // Use $ in variable naming to indicate presence in DOM
-let $createBtn
-let $updateBtn
-let $usernameFld, $passwordFld, $firstNameFld, $lastNameFld, $roleFld
-const userService = new AdminUserServiceClient()
+    let $createBtn
+    let $updateBtn
+    let $usernameFld, $passwordFld, $firstNameFld, $lastNameFld, $roleFld
+    const userService = new AdminUserServiceClient()
 
-const deleteUserFromTable = (event) => {
-    const deleteBtn = $(event.currentTarget)
-    // deleteBtn.parent().parent().parent().remove()
-    deleteBtn.parents("tr.wbdv-template").remove()
-    console.log("delete user from table")
-}
+    const deleteUserFromTable = (event) => {
+        const deleteBtn = $(event.currentTarget)
+        // deleteBtn.parent().parent().parent().remove()
+        deleteBtn.parents("tr.wbdv-template").remove()
+        console.log("delete user from table")
+    }
 
-const renderUsers = (users) => {
-    tbody.empty()
-    for(let i=0; i<users.length; i++) {
-        user = users[i]
+    const renderUser = (user) => {
+        tbody.empty()
         clone = template.clone()
         // swap wbdv-hidden for hide class
         clone.removeClass("wbdv-hidden")
         clone.find(".wbdv-username").html(user.username)
         clone.find(".wbdv-first-name").html(user.firstname)
         clone.find(".wbdv-last-name").html(user.lastname)
+        clone.find(".wbdv-role").html(user.role)
         // clone.find(".wbdv-remove").click(deleteUserFromTable)
         clone.find(".wbdv-remove").click(() => removeUserFromArray(i))
         clone.find(".wbdv-edit").click(() => editUser(i))
         tbody.append(clone)
     }
-}
 
-const removeUserFromArray = (_index) => {
-    //console.log(index)
-    const user = users[_index]
-    const userID = user._id
-    userService.deleteUser(userID)
-        .then(response => {
-            users.splice(_index, 1)
-            renderUsers(users)
-            console.log("remove user from array")
-        })
-}
-
-const generateID = () => {
-    const prefix = new Date().getFullYear()
-    const suffix = Math.floor(Math.random() * (1000 - 0) + 0)
-    const id = "" + prefix + suffix
-    return id
-}
-
-const createUser = () => {
-    console.log("Create user!")
-    const username = $usernameFld.val()
-    // const password = $passwordFld.val()
-    const firstname = $firstNameFld.val()
-    const lastname = $lastNameFld.val()
-    const role = $roleFld.val()
-
-    // Can use same field for read/write depending on passed args
-    $usernameFld.val("")
-    $passwordFld.val("")
-    $firstNameFld.val("")
-    $lastNameFld.val("")
-    $roleFld.val("")
-
-    const newUser = {
-        // userid: generateID(),
-        username: username,
-        firstname: firstname,
-        lastname: lastname,
-        role: role
+    const renderUsers = (users) => {
+        tbody.empty()
+        for (let i = 0; i < users.length; i++) {
+            user = users[i]
+            clone = template.clone()
+            // swap wbdv-hidden for hide class
+            clone.removeClass("wbdv-hidden")
+            clone.find(".wbdv-username").html(user.username)
+            clone.find(".wbdv-first-name").html(user.firstname)
+            clone.find(".wbdv-last-name").html(user.lastname)
+            clone.find(".wbdv-role").html(user.role)
+            // clone.find(".wbdv-remove").click(deleteUserFromTable)
+            clone.find(".wbdv-remove").click(() => removeUserFromArray(i))
+            clone.find(".wbdv-edit").click(() => editUser(i))
+            tbody.append(clone)
+        }
     }
 
-    userService.createUser(newUser)
-        .then(actualNewUser => {
-            users.push(actualNewUser)
-            renderUsers(users)
+    const removeUserFromArray = (_index) => {
+        //console.log(index)
+        const user = users[_index]
+        const userID = user._id
+        userService.deleteUser(userID)
+            .then(response => {
+                users.splice(_index, 1)
+                renderUsers(users)
+                console.log("remove user from array")
+            })
+    }
+
+    const generateID = () => {
+        const prefix = new Date().getFullYear()
+        const suffix = Math.floor(Math.random() * (1000 - 0) + 0)
+        const id = "" + prefix + suffix
+        return id
+    }
+
+    const createUser = () => {
+        console.log("Create user!")
+        const username = $usernameFld.val()
+        // const password = $passwordFld.val()
+        const firstname = $firstNameFld.val()
+        const lastname = $lastNameFld.val()
+        const role = $roleFld.val()
+
+        // Can use same field for read/write depending on passed args
+        $usernameFld.val("")
+        $passwordFld.val("")
+        $firstNameFld.val("")
+        $lastNameFld.val("")
+        $roleFld.val("")
+
+        const newUser = {
+            // userid: generateID(),
+            username: username,
+            firstname: firstname,
+            lastname: lastname,
+            role: role
+        }
+
+        userService.createUser(newUser)
+            .then(actualNewUser => {
+                users.push(actualNewUser)
+                renderUsers(users)
+            })
+    }
+
+    const editUser = (index) => {
+        console.log("Edit user!")
+
+        selectedUser = users[index]
+
+        console.log(selectedUser.username)
+
+        $usernameFld.val(selectedUser.username)
+        $passwordFld.val("")
+        $firstNameFld.val(selectedUser.firstname)
+        $lastNameFld.val(selectedUser.lastname)
+        $roleFld.val(user.role)
+
+    }
+
+    const updateUser = (event) => {
+        console.log("Edit user!")
+        const updateUsername = $usernameFld.val()
+        // const password = $passwordFld.val()
+        const updateFirstName = $firstNameFld.val()
+        const updateLastName = $lastNameFld.val()
+        const updateRole = $roleFld.val()
+        const userID = selectedUser._id
+
+        userService.updateUser(userID, {
+            username: updateUsername,
+            firstname: updateFirstName,
+            lastname: updateLastName,
+            role: updateRole
         })
-}
+            .then(response => {
+                selectedUser.username = updateUsername
+                selectedUser.firstname = updateFirstName
+                selectedUser.lastname = updateLastName
+                selectedUser.role = updateRole
+                renderUsers(users)
+            })
 
-const editUser = (index) => {
-    console.log("Edit user!")
-
-    selectedUser = users[index]
-
-    console.log(selectedUser.username)
-
-    $usernameFld.val(selectedUser.username)
-    $passwordFld.val("")
-    $firstNameFld.val(selectedUser.firstname)
-    $lastNameFld.val(selectedUser.lastname)
-    $roleFld.val(user.role)
-
-}
-
-const updateUser = (event) => {
-    console.log("Edit user!")
-    const updateUsername = $usernameFld.val()
-    // const password = $passwordFld.val()
-    const updateFirstName = $firstNameFld.val()
-    const updateLastName = $lastNameFld.val()
-    const updateRole = $roleFld.val()
-    const userID = selectedUser._id
-
-    userService.updateUser(userID, {
-        username: updateUsername,
-        firstname: updateFirstName,
-        lastname: updateLastName,
-        role: updateRole
-    })
-        .then(response => {
-            selectedUser.username = updateUsername
-            selectedUser.firstname = updateFirstName
-            selectedUser.lastname = updateLastName
-            selectedUser.role = updateRole
-            renderUsers(users)
-        })
-
-    // Can use same field for read/write depending on passed args
-    $usernameFld.val("")
-    $passwordFld.val("")
-    $firstNameFld.val("")
-    $lastNameFld.val("")
-    $roleFld.val("")
+        // Can use same field for read/write depending on passed args
+        $usernameFld.val("")
+        $passwordFld.val("")
+        $firstNameFld.val("")
+        $lastNameFld.val("")
+        $roleFld.val("")
 
 
-}
+    }
 
-const init = () => {
+    const init = () => {
 
-    tbody = $("tbody")
-    template = $("tr.wbdv-template")
-    $createBtn = $(".wbdv-create").click(createUser)
-    $updateBtn = $(".wbdv-update").click(updateUser)
-    $usernameFld = $("#usernameFld")
-    $passwordFld = $("#passwordFld")
-    $firstNameFld = $("#firstNameFld")
-    $lastNameFld = $("#lastNameFld")
-    $roleFld = $("#roleFld")
+        tbody = $("tbody")
+        template = $("tr.wbdv-template")
+        $createBtn = $(".wbdv-create").click(createUser)
+        $updateBtn = $(".wbdv-update").click(updateUser)
+        $usernameFld = $("#usernameFld")
+        $passwordFld = $("#passwordFld")
+        $firstNameFld = $("#firstNameFld")
+        $lastNameFld = $("#lastNameFld")
+        $roleFld = $("#roleFld")
 
-    //renderUsers(users)
-
-    userService.findAllUsers()
-        .then((_users) => {
-            console.log(_users)
-            users = _users
-            renderUsers(users)
-        })
-}
-$(init)
+        userService.findAllUsers()
+            .then((_users) => {
+                console.log(_users)
+                users = _users
+                renderUsers(users)
+            })
+    }
+    $(init)
 
 // <tr class="wbdv-template wbdv-user wbdv-hidden">
 //     <td class="wbdv-username">wonderwoman</td>
@@ -190,3 +205,4 @@ $(init)
 //     </span>
 // </td>
 // </tr>
+})();
