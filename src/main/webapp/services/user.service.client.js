0528badce61afc6acb1804bdd70f2c8efefd03e7
Will Cohen <will.r.cohen@gmail.com>
2020-10-01 22:05:13 -0400

    chore: Update sever user account to cohenw

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -5,7 +5,7 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/wcohen/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/cohenw/users';
     let self = this;
     function findAllUsers() {
         return fetch('https://wbdv-generic-server.herokuapp.com/api/wcohen/users')
