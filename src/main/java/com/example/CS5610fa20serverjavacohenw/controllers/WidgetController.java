2ce083ca135311c0c4c2e7e0181aed9d7431e57d
Will Cohen <will.r.cohen@gmail.com>
2020-10-21 17:10:30 -0400

    feat: Add widget controller and CRUD definitions

---
+++ src/main/java/com/example/CS5610fa20serverjavacohenw/controllers/WidgetController.java
@@ -0,0 +1,25 @@
+package com.example.CS5610fa20serverjavacohenw.controllers;
+
+import com.example.CS5610fa20serverjavacohenw.models.Widget;
+
+import java.util.List;
+
+public class WidgetController {
+    public List<Widget> findAllWidgets() {
+        return null;
+    }
+
+    public Widget findWidgetById(Integer widgetId) {
+        return null;
+    }
+
+    public Widget createWidget(Widget widget) {
+        return null;
+    }
+
+    public void deleteWidget(Integer widgetId) {}
+
+    public Widget updateWidget(Widget widget) {
+        return null;
+    }
+}
